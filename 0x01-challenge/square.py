be45cae29cc5d86f1ca543d7c49064cf25cd496e
Mduduzi Silinda <36817306+SilindaM@users.noreply.github.com>
2022-09-29 12:01:55 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+"""Main program"""
 
 
-class Square():
-
+class Square:
+    """Defines the square class"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Constructor"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
+        """Returns the perimeter"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        """Override of __str__"""
+        return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
